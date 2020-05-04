731d8683e1fca258cd3950dc260072a62db5684f
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-05-04 22:52:55 +0900

    Create brute_force.cpp

---
+++ backjoon/[14888]연산자 끼워넣기/brute_force.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+const int INF=987654321;
+int n,num[11];
+//순서:[0]plus,[1]minus,[2]multiply,[3]divide
+int operationsNum[4];
+
+void init(void)
+{
+    cin>>n;
+    for(int i=0;i<n;++i)
+        cin>>num[i];
+    for(int i=0;i<4;++i)
+        cin>>operationsNum[i];
+}
+
+int calcUsingOperator(int frontRet, int index, int operatorIdx)
+{
+    int ret;
+    switch(operatorIdx)
+    {
+        case 0: ret=frontRet+num[index]; break;
+        case 1: ret=frontRet-num[index]; break;
+        case 2: ret=frontRet*num[index]; break;
+        case 3: ret=frontRet/num[index]; break;
+    }
+    return ret;
+}
+
+int getMaxRet(int begin)
+{
+    if(begin==0)
+        return num[begin];
+    
+    int ret=INF*-1;
+    for(int i=0;i<4;++i)
+        if(operationsNum[i]>0)
+        {
+            --operationsNum[i];
+            int candidate=calcUsingOperator(getMaxRet(begin-1),begin,i);
+            ret=max(ret,candidate);
+            ++operationsNum[i];
+        }
+    
+    return ret;
+}
+
+int getMinRet(int begin)
+{
+    if(begin==0)
+        return num[begin];
+    
+    int ret=INF;
+    for(int i=0;i<4;++i)
+        if(operationsNum[i]>0)
+        {
+            --operationsNum[i];
+            int candidate=calcUsingOperator(getMinRet(begin-1),begin,i);
+            ret=min(ret,candidate);
+            ++operationsNum[i];
+        }
+    
+    return ret;
+}
+
+int main(void)
+{
+    init();
+    
+    cout<<getMaxRet(n-1)<<endl<<getMinRet(n-1);
+}
