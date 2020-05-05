cef0c7fbf32e704ae902a42366951104948ab59e
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-05-05 22:22:48 +0900

    Create  [1149]RGB거리.cpp

---
+++ backjoon/[1149]RGB거리/[1149]RGB거리.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int INF=987654321;
+int n,cost[1001][3];
+int cache[1001][4];
+
+void init(void)
+{
+    cin>>n;
+    
+    for(int i=0;i<n;++i)
+        for(int j=0;j<3;++j)
+            cin>>cost[i][j];
+    memset(cache,-1,sizeof(cache));
+}
+
+int getMinSum(int here, int previousColor)
+{
+    if(here==n)
+        return 0;
+    
+    int& ret=cache[here][previousColor];
+    if(ret!=-1)
+        return ret;
+    
+    ret=INF;
+    for(int color=0;color<3;++color)
+        if(previousColor!=color)
+            ret=min(ret,cost[here][color]+getMinSum(here+1,color));
+    
+    return ret;
+}
+
+int main(void)
+{
+    init();
+    
+    cout<<getMinSum(0,3);
+    
+    return 0;
+}
