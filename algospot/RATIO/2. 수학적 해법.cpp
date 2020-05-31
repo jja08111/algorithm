6907ef0dd84b013eae80f7581ba3439d04928fa7
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-05-31 16:04:19 +0900

    Create 2. 수학적 해법.cpp

---
+++ algospot/RATIO/2. 수학적 해법.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const long long MAX_N=2000000000;
+
+int getRatio(long long total, long long win)
+{
+    return (win*100)/total;
+}
+
+int neededGames(long long total, long long win)
+{
+    long long ratio=getRatio(total,win)+1;
+    
+    if(ratio>=100)
+        return -1;
+    
+    double ret=(double)(ratio*total-100*win)/(100-ratio);
+    int temp=(int)ret;
+    
+    if(ret-(double)temp>0.0)
+        return ret+1;
+    return ret;
+}
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+    while(testCase--)
+    {
+        int total,win;
+        cin>>total>>win;
+        
+        cout<<neededGames(total,win)<<endl;
+    }
+    return 0;
+}
