71185f6e3f18325ca77453ca4e9a8685a5d1df89
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-06-13 22:26:19 +0900

    Update and rename dfs.cpp to bitmask.cpp

--- backjoon/[14889]스타트와 링크/bitmask.cpp
+++ backjoon/[14889]스타트와 링크/bitmask.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 const int INF=987654321;
 
-int n;
-int ability[20][20];
+int n,ability[20][20];
 int ret=INF;
 
 void getMinDiff(int team)
@@ -15,8 +13,10 @@ void getMinDiff(int team)
     for(int a=0;a<n;++a)
         for(int b=a+1;b<n;++b)
         {
+            // a선수와 b선수가 start팀에 속하는 경우
             if(((1<<a)&team) && ((1<<b)&team))
                 startAbility+=(ability[a][b]+ability[b][a]);
+            // a선수와 b선수가 link팀에 속하는 경우
             else if(!((1<<a)&team) && !((1<<b)&team))
                 linkAbility+=(ability[a][b]+ability[b][a]);
         }
@@ -24,21 +24,32 @@ void getMinDiff(int team)
     ret=min(ret,abs(startAbility-linkAbility));
 }
 
-void solve(int visited)
+// 비트 마스크를 이용
+void solve(int selected)
 {
-    if(__builtin_popcount(visited)==n/2)
-        getMinDiff(visited);
-
-    int here=0;
-    for(int i=0;i<n;++i)
-        if(visited&(1<<i))
-            here=i;
+    if(ret==0) return;
+    // 선수 선택을 완료한 경우
+    if(__builtin_popcount(selected)==n/2)
+    {
+        getMinDiff(selected);
+        return;
+    }
     
+    // 중복을 막기 위해 가장 번호가 큰 선택된 선수를 찾는다.
+    int here=0;
+    for(int i=n-1;i>=0;--i)
+        if(selected&(1<<i))
+        {
+            here=i;
+            break;
+        }
+    
+    // dfs
     for(int i=here+1;i<n;++i)
     {
         int next=(1<<i);
-        if(!(visited&next))
-            solve(visited|next);
+        if(!(selected&next))
+            solve(selected|next);
     }
 }
 
@@ -49,6 +60,7 @@ int main()
         for(int j=0;j<n;++j)
             cin>>ability[i][j];
     
+    // 0번 선수가 있는 팀과 없는 팀으로 나눠서 해결
     solve(1);
     cout<<ret;
     
