2b25f2f3204be1bbb92b098d0c9309ac222db53a
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-06-19 22:28:27 +0900

    Update and rename test.cpp to greedy.cpp

---
+++ backjoon/[1912]연속합/greedy.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int NAG_INF=-987654321;
+const int MAX=100000;
+
+int n,seq[MAX];
+int answer=NAG_INF;
+
+int solve(int start)
+{
+    if(start==n)
+        return 0;
+    
+    //현재 숫자를 포함한다.
+    int ret=seq[start];
+    //다음 숫자부터 시작하는 수열들 중 최대합을 찾는다.
+    int next=solve(start+1);
+    //다음 수열이 음수인 경우 포함하지 않는다.
+    ret+=max(0,next);
+    
+    //최대값을 전역변수에 저장하여 따로 반복문을 만들지 않는다.
+    answer=max(answer,ret);
+    
+    return ret;
+}
+
+int main()
+{
+    cin>>n;
+    for(int i=0;i<n;++i)
+        cin>>seq[i];
+    
+    solve(0);
+    cout<<answer;
+    return 0;
+}
