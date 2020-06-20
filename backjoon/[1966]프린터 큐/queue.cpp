ae3fe2f2ef1d07c781f96405aa95870f25848280
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-06-20 23:11:33 +0900

    Update queue.cpp

--- backjoon/[1966]프린터 큐/queue.cpp
+++ backjoon/[1966]프린터 큐/queue.cpp
@@ -7,8 +7,10 @@ int getOrder(priority_queue<int>& priority, queue<pair<int,int> >& document, int
     int count=1;
     while(1)
     {
+        //현재 문서의 우선순위가 제일 높을 때
         if(priority.top()==document.front().second)
         {
+            //현재 문서가 출력 순서를 찾는 문서일 때
             if(document.front().first==m)
                 return count;
             priority.pop();
@@ -16,7 +18,8 @@ int getOrder(priority_queue<int>& priority, queue<pair<int,int> >& document, int
             count++;
         }
         else
-        {
+        {   
+            //문서를 제일 뒤로 보낸다.
             pair<int,int> tmp=document.front();
             document.pop();
             document.push(tmp);
