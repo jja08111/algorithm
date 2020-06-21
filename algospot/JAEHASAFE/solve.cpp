5557abda8e83f285b2838ef675f0270c270582fa
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-06-21 20:25:37 +0900

    Create solve.cpp

---
+++ algospot/JAEHASAFE/solve.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int INF=987654321;
+
+vector<int> getPartialMatch(const string& s)
+{
+    int n=s.size();
+    vector<int> pi(n,0);
+    int begin=1, matched=0;
+    while(begin+matched<n)
+    {
+        if(s[begin+matched]==s[matched])
+        {
+            matched++;
+            pi[begin+matched-1]=matched;
+        }
+        else
+        {
+            if(matched==0)
+                begin++;
+            else
+            {
+                begin+=matched-pi[matched-1];
+                matched=pi[matched-1];
+            }
+        }
+    }
+    return pi;
+}
+
+vector<int> kmpSearch(const string& a, const string& b)
+{
+    int n=a.size(), m=b.size();
+    vector<int> ret, pi=getPartialMatch(b);
+    int begin=0, matched=0;
+    while(begin<=n-m)
+    {
+        if(matched<m && a[begin+matched]==b[matched])
+        {
+            matched++;
+            if(matched==m)
+                ret.push_back(begin);
+        }
+        else
+        {
+            if(matched==0)
+                begin++;
+            else
+            {
+                begin+=matched-pi[matched-1];
+                matched=pi[matched-1];
+            }
+        }
+    }
+    return ret;
+}
+
+int shifts(const string& original, const string& target)
+{
+    return kmpSearch(original+original,target)[0];
+}
+
+int main()
+{
+    int testCase;
+    cin>>testCase;
+    while(testCase--)
+    {
+        string a,b;
+        int n,ret=0;
+        cin>>n;
+        for(int i=0;i<n;++i)
+        {
+            if(i==0)
+                cin>>b;
+            
+            if(i%2==0)
+                cin>>a;
+            else
+                cin>>b;
+            
+            ret+=shifts(a,b);
+        }
+        cout<<ret<<endl;
+    }
+    
+    return 0;
+}
