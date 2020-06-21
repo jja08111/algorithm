fd31142fbc44993f748ef27cf85dcde8a25155e2
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-06-21 11:55:25 +0900

    Create KMP_algorithm.cpp

---
+++ algospot/NAMING/KMP_algorithm.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+vector<int> getPartialMatch(const string& s)
+{
+    int n=s.size();
+    vector<int> pi(n,0);
+    int begin=1, matched=0;
+    while(begin+matched<n)
+    {
+        if(s[matched]==s[begin+matched])
+        {
+            matched++;
+            pi[begin+matched-1]=matched;
+        }
+        else
+        {
+            if(matched==0)
+                begin++;
+            else
+            {
+                begin+=matched-pi[matched-1];
+                matched=pi[matched-1];
+            }
+        }
+    }
+    return pi;
+}
+
+vector<int> getPrefixSuffix(const string& s)
+{
+    vector<int> ret, pi=getPartialMatch(s);
+    int k=s.size();
+    while(k>0)
+    {
+        ret.push_back(k);
+        k=pi[k-1];
+    }
+    return ret;
+}
+
+int main()
+{
+    string father,mother;
+    cin>>father>>mother;
+    string s=father+mother;
+    
+    vector<int> ret=getPrefixSuffix(s);
+    sort(ret.begin(),ret.end());
+    for(int i=0;i<ret.size();++i)
+        cout<<ret[i]<<" ";
+    
+    return 0;
+}
