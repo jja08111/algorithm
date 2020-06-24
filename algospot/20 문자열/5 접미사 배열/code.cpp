8dbbaa2550851ab51c2435b8589a36d015fd1232
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-06-24 22:39:48 +0900

    Update code.cpp

--- algospot/20 문자열/5 접미사 배열/code.cpp
+++ algospot/20 문자열/5 접미사 배열/code.cpp
@@ -5,30 +5,33 @@
 using namespace std;
 
 struct Comparator {
-    const vector<int>& group;
+    vector<int> group;
     int t;
-    Comparator(const vector<int>& _group, int _t): group(_group), t(_t) {
-        group=_group; t=_t;
-    }
-    bool operator () (int a, int b) {
+    Comparator(const vector<int>& _group, int _t) : group(_group), t(_t) {}
+    bool operator () (int a, int b)
+    {
         if(group[a]!=group[b])
             return group[a]<group[b];
         return group[a+t]<group[b+t];
     }
 };
 
-vector<int> getSuffixArray(const string& s)
+vector<int> getSuffixArray(const vector<int>& s)
 {
     int n=s.size();
     int t=1;
+    
+    //첫 번째 글자를 기준으로 그룹화
     vector<int> group(n+1);
     for(int i=0;i<n;++i)
         group[i]=s[i];
     group[n]=-1;
     
+    //접미사 배열이 될 값
     vector<int> perm(n);
     for(int i=0;i<n;++i)
         perm[i]=i;
+    
     while(t<n)
     {
         Comparator compareUsing2T(group,t);
@@ -43,11 +46,10 @@ vector<int> getSuffixArray(const string& s)
         newGroup[n]=-1;
         newGroup[perm[0]]=0;
         for(int i=1;i<n;++i)
-            if(compareUsing2T(perm[i-1], perm[i]))
-                newGroup[perm[i]]=newGroup[perm[i-1]]+1;
+            if(compareUsing2T(perm[i-1],perm[i]))
+                newGroup[i]=newGroup[i-1]+1;
             else
-                newGroup[perm[i]]=newGroup[perm[i-1]];
-        
+                newGroup[i]=newGroup[i-1];
         group=newGroup;
     }
     return perm;
@@ -55,5 +57,9 @@ vector<int> getSuffixArray(const string& s)
 
 int main()
 {
+    string s="mississipi";
+    vector<int> ret=getSuffixArray(s);
+    cout<<ret[0];
+    
     return 0;
 }
