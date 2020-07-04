0176f3722c0d04b14c387fc070ed30c847d580fe
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-07-04 11:01:20 +0900

    Create solve.cpp

---
+++ backjoon/[1991]트리 순회/solve.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+char node[26][2]={'.',};
+
+void preorder(char root)
+{
+    if(root=='.')
+        return;
+    cout<<root;
+    preorder(node[root-'A'][0]);
+    preorder(node[root-'A'][1]);
+}
+
+void inorder(char root)
+{
+    if(root=='.')
+        return;
+    inorder(node[root-'A'][0]);
+    cout<<root;
+    inorder(node[root-'A'][1]);
+}
+
+void postorder(char root)
+{
+    if(root=='.')
+        return;
+    postorder(node[root-'A'][0]);
+    postorder(node[root-'A'][1]);
+    cout<<root;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i)
+    {
+        char tmp;
+        cin>>tmp;
+        cin>>node[tmp-'A'][0]>>node[tmp-'A'][1];
+    }
+    
+    preorder('A');
+    cout<<endl;
+    inorder('A');
+    cout<<endl;
+    postorder('A');
+    cout<<endl;
+
+    return 0;
+}
