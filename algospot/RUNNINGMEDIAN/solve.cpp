2d12e36fd759ed6cc1ad0f96b5cd90336ab4c832
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-07-05 22:42:33 +0900

    Update solve.cpp

--- algospot/RUNNINGMEDIAN/solve.cpp
+++ algospot/RUNNINGMEDIAN/solve.cpp
@@ -17,10 +17,17 @@ struct RNG {
 
 int runningMedian(int n, RNG rng)
 {
+    //중간값과 같거나 작은 값들을 내림차순으로 저장
+    //top에는 제일 큰 값이 있다.
     priority_queue<int, vector<int>, less<int> > maxHeap;
+    //중간값보다 큰 값들을 오름차순으로 저장
+    //top에는 제일 작은 값이 있다.
     priority_queue<int, vector<int>, greater<int> > minHeap;
     int ret=0;
     
+    //반복문 불변식:
+    //1.maxHeap의 크기는 minHeap의 크기와 같거나 1 더 크다.
+    //2.maxHeap.top() <= minHeap.top()
     for(int cnt=1;cnt<=n;++cnt)
     {
         if(maxHeap.size()==minHeap.size())
