97ff79e2fb275a0088f340346009aa38fe06968f
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-07-05 13:43:20 +0900

    Create solve1_using_set.cpp

---
+++ backjoon/[1167]트리의 지름/solve1_using_set.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <set>
+using namespace std;
+
+const int MAX=100001;
+
+// adj[here]=(there,dist)
+vector<pair<int,int> > adj[MAX];
+bool visited[MAX];
+// 잎과 잎 사이의 거리 중 가장 긴 값
+int longest=0;
+
+int getDiameter(int here)
+{
+    visited[here]=true;
+    
+    set<int> ret;
+    for(int i=0;i<adj[here].size();++i)
+    {
+        int cand=adj[here][i].first;
+        int dist=adj[here][i].second;
+        if(!visited[cand])
+        {
+            ret.insert(dist+getDiameter(cand));
+        }
+    }
+    
+    if(ret.empty())
+        return 0;
+        
+    set<int>::iterator it=ret.end();
+    int back = *(--it);
+    if(ret.size()>=2)
+    {
+        int back2 = *(--it);
+        longest=max(longest,back+back2);
+    }
+    return back;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    for(int i=0;i<n;++i)
+    {
+        int here;
+        cin>>here;
+        while(true)
+        {
+            int there, dist;
+            
+            scanf("%d",&there);
+            if(there==-1) 
+                break;
+            scanf("%d",&dist);
+            
+            adj[here].push_back(make_pair(there,dist));
+        }
+    }
+    
+    //잎-잎 거리와 루트-잎 거리 중 더 큰 값을 출력
+    int h=getDiameter(1);
+    int ret=max(h,longest);
+    cout<<ret;
+    
+    return 0;
+}
