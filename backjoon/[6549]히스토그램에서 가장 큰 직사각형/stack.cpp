971d4e66f1d08f0076418494cdcb6c865fb71f02
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-07-07 16:41:24 +0900

    Create stack.cpp

---
+++ backjoon/[6549]히스토그램에서 가장 큰 직사각형/stack.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> h;
+int n;
+
+long long getMaxArea()
+{
+    stack<int> remaining;
+    h.push_back(-1);
+    long long int ret=0;
+    for(int i=0;i<h.size();++i)
+    {
+        while(!remaining.empty() && h[remaining.top()]>=h[i])
+        {
+            int j=remaining.top();
+            remaining.pop();
+            int width=-1;
+            
+            if(remaining.empty())
+                width=i;
+            else
+                width=(i-remaining.top()-1);
+            ret=max(ret,(long long)width*h[j]);
+        }
+        remaining.push(i);
+    }
+    return ret;
+}
+
+int main()
+{
+    while(true)
+    {
+        cin>>n;
+        if(n==0)
+            break;
+        
+        h.clear();
+        for(int i=0;i<n;++i)
+        {
+            int tmp;
+            scanf("%d",&tmp);
+            h.push_back(tmp);
+        }
+        cout<<getMaxArea()<<endl;
+    }
+
+    return 0;
+}
