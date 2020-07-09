2cc79be5e4b69cda3780598dfa2da148f9646dd7
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-07-09 23:38:15 +0900

    Update test.cpp

--- backjoon/[12852]1로 만들기 2/test.cpp
+++ backjoon/[12852]1로 만들기 2/test.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -58,7 +50,7 @@ int main()
     int idx=n;
     while(true)
     {
-        cout<<idx<<endl;
+        cout<<idx<<' ';
         if(idx==1)
             break;
         idx=previous[idx];
