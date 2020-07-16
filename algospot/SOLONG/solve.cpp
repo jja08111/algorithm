20fe3564c4cb511f50eed9d5867d143cef55d251
jja08111 <57604817+jja08111@users.noreply.github.com>
2020-07-16 23:04:08 +0900

    Update solve.cpp

--- algospot/SOLONG/solve.cpp
+++ algospot/SOLONG/solve.cpp
@@ -22,14 +22,18 @@ struct TrieNode {
                 delete children[i];
     }
     void insert(const char* key, int id) {
+        // 첫 번째 문자가 등록 되지 않았다면 갱신
         if(first==-1)
             first=id;
+        // 문자열이 끝났다면 terminal 갱신
         if(*key==0)
             terminal=id;
         else {
             int next=toNumber(*key);
+            // 해당 자식 노드가 없다면 생성
             if(children[next]==NULL)
                 children[next]=new TrieNode();
+            // 재귀 호출
             children[next]->insert(key+1,id);
         }
     }
@@ -41,11 +45,14 @@ struct TrieNode {
             return NULL;
         return children[next]->find(key+1);
     }
+    // 번호가 id인 key를 타이핑 하기 위한 최소 회수 반환
     int type(const char* key, int id) {
         if(*key==0)
             return 0;
+        // 이 노드에서 추천되는 문자열이 이 문자열이면 탭 누르고 종료
         if(first==id)
             return 1;
+        // 아니면 다음 문자를 타이핑한다.
         int next=toNumber(*key);
         return 1+children[next]->type(key+1,id);
     }
@@ -54,8 +61,10 @@ struct TrieNode {
 int countKeys(TrieNode* trie, const char* word)
 {
     TrieNode* node=trie->find(word);
+    // 문자열이 사전에 없는 경우 직접 타이핑
     if(node==NULL || node->terminal==-1)
         return strlen(word);
+    // 탐색하면서 타이핑할 방법을 찾는다.  
     return trie->type(word,node->terminal);
 }
 
