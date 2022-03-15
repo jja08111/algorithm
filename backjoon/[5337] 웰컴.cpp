f12799cfb31cddb8847ef9222a5dd4b04eb09ad8
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-03-15 23:42:27 +0900

    Create [5337] 웰컴.cpp

---
+++ backjoon/[5337] 웰컴.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cstring>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <cctype>
+using namespace std;
+
+const int INF = 987654321;
+const int MOD = 1000000000 + 7;
+const int DIR[4][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+
+// ------------------------------------------------------
+
+int main(void) {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+    cout << ".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/." << endl;
+
+    return 0;
+}
