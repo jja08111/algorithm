27d6ef66557316e46d7d3754d7e1a55f0a0246f3
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-03-20 23:53:05 +0900

    Create [6749] Next in line.cpp

---
+++ backjoon/[6749] Next in line.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cstring>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <cctype>
+using namespace std;
+
+const int INF = 987654321;  
+const int MOD = 1000000000 + 7;
+const int DIR[4][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+
+// ------------------------------------------------------
+
+int main(void) {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << b + (b - a) << endl;
+
+    return 0;
+}
